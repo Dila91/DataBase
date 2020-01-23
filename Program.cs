b4f782ef3eb63652c1c6b5adca9e6b943de0dd95
Dila91 <59875110+Dila91@users.noreply.github.com>
2020-01-23 15:38:40 +0100

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace DataBase
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    //test
                 });
     }
 }
